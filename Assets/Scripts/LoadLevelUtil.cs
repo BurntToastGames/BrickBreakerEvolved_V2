1fa91ab53fc2a7497225eb82f9d96b8aaee96ae2
Henry Kucab <u0918222@1120-mac-29.lib.utah.edu>
2016-10-31 19:53:51 -0600

    Attempt 2 at V2. Changed Blocks_assets to Blocks

---
+++ Assets/Scripts/LoadLevelUtil.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevelUtil : MonoBehaviour {
+
+	public void LoadLevel (int level)
+    {
+		UnityEngine.SceneManagement.SceneManager.LoadScene(level);
+	}
+
+	public void ExitGame (){
+		Application.Quit();
+	}
+}
