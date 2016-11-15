22308d384dbdc700ffbcb71763c1ded560b320c9
Mytholl <walteralitalley@gmail.com>
2016-11-14 18:19:42 -0700

    Added title scene
    Title Scene has been added along with the 1 script to run it. also
    jpg’s for the images

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+
+    public void NewGameBtn(string newGameLevel)
+    {
+        SceneManager.LoadScene(newGameLevel);
+
+    }
+
+    public void ExitGameBtn()
+    {
+    Application.Quit();
+        }
+}
